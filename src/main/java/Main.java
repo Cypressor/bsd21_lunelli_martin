bb25977e4562620bcb1d156448e101b5d676cf32
Martin Lunelli <martin.lunelli@gmail.com>
2022-05-22 21:18:09 +0200

    exercise 5 2nd commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,24 @@
 import at.campus02.bsd.Calculator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class Main
 {
+    private static Logger logger = LogManager.getLogger();
+
     public static void main(String[] args)
     {
-        Calculator calculator = new Calculator();
-        System.out.printf("%f",calculator.add(1,2)+"\n");
-        System.out.printf("%f",calculator.minus(1,2)+"\n");
-        System.out.printf("%f",calculator.multiply(1,2)+"\n");
-        System.out.printf("%f",calculator.divide(1,2)+"\n");
-        System.out.printf("%f",calculator.faculty(1)+"\n");
+       /* Calculator calculator = new Calculator();
+        System.out.printf("%f\n",calculator.add(1,2));
+        System.out.printf("%f\n",calculator.minus(1,2));
+        System.out.printf("%f\n",calculator.multiply(1,2));
+        System.out.printf("%f\n",calculator.divide(1,2));
+        System.out.printf("%d\n",calculator.faculty(1));
+*/
+        logger.info("INFO");
+        logger.error("ERROR");
+
+
     }
 }
